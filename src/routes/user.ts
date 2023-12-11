b9a1eba4ee32018eb7a259fe68994851cf4665d0
FarzamN <89484118+FarzamN@users.noreply.github.com>
2023-12-12 00:04:12 +0500

    login api and token is done

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,8 @@
-import {Router, Request, Response} from 'express';
-import {userModule} from '../models/user';
+import {Router, Request, Response, NextFunction} from 'express';
+import {IUser, userModule} from '../models/user';
 import {UserError} from '../Errors';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const router = Router();
 
@@ -22,5 +23,42 @@ router.post('/register', async (req: Request, res: Response) => {
     res.status(500).json({type: 'catch error'});
   }
 });
+
+router.post('/login', async (req: Request, res: Response) => {
+  const {username, password} = req.body;
+  try {
+    const user = await userModule.findOne({username});
+    if (!user) {
+      return res.status(400).json({type: UserError.NO_USER_FOUND});
+    }
+
+    const isPassword = await bcrypt.compare(password, user.password);
+    if (!isPassword) {
+      return res.status(400).json({type: UserError.WRONG_CREDENTIALS});
+    }
+    const token = jwt.sign({id: user.id}, 'secret');
+    res.json({token, userID: user.id});
+  } catch (error) {
+    console.error('An error occurred:', error);
+    res.status(500).json({type: 'catch error'});
+  }
+});
+
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+  if (authHeader) {
+    jwt.verify(authHeader, 'secret', err => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+      return res.sendStatus(401);
+    });
+  } else {
+  }
+};
 
 export {router as userRouter};
